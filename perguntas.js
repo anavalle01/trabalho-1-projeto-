f9d9e788f81de3d7fa62b1372a69cc0edae25648
anavalle01 <ana.vale@escola.pr.gov.br>
2024-10-01 08:02:22 -0300

    Add files via upload
    3 tri

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Matemática',
+    'O que é função afim??',
+    'Qualquer função que possua a lei de formação Y = AX + B, sendo A e B números reais e diferentes de 0.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são tecnopolos?',
+    'Espaços que concentram atividade e pesquisa e desenvolvimento em áreas de alta tecnologia.'
+)
+
+criaCartao(
+    'Química',
+    'Quais são os tipos de cadeias carbônicas?',
+    'Homogênea - Heterogênea, Saturada - Insaturada, Fechadas - Abertas, Normal - Ramificada.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como você está em Inglês?',
+    'como você está em ingles é How are you? '
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quais são os principais temas abordados no Romantismo?',
+    'Individualismo, Nacionalismo, Exaltação do amor, Fuga da realidade, Morte melancólica.'
+)
+
+criaCartao(
+    'História' , 
+    'O que marcou a Era Vargas?' ,
+    'A consolidação das Leis Trabalhistas e a censura imingida na mídia e na arte.'
+)
+
+criaCartao(
+       'Biologia'
+    'Qual foi a primeira Lei de Mendel?'
+    'Cada característica e indivíduos é determinada por um par de genes se separam durante a formação dos gametas. Pai e mãe transmitem apenas um gene para seus descentes.'
+)
